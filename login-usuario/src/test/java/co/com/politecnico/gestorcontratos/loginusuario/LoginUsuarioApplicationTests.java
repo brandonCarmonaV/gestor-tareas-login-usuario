c1fd11d5352f71e1ee687549b568f0b814c94964
Brandon Carmona <brandon.carmona@quipux.com>
2026-08-27 07:48:25 -0500

    commit inicial con la estructura del proyecto

---
+++ login-usuario/src/test/java/co/com/politecnico/gestorcontratos/loginusuario/LoginUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.politecnico.gestorcontratos.loginusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
